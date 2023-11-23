e22fa6c462c4f134f2b1fb93a1bc95bea6d92376
KunstnerDrengen <89451801+KunstnerDrengen@users.noreply.github.com>
2023-11-23 13:06:31 +0100

    sound update

--- Assets/Scripts/Audio_Man.cs
+++ Assets/Scripts/Audio_Man.cs
@@ -10,6 +10,7 @@ public class Audio_Man : MonoBehaviour
 
     [Header("Audio clips")]
     public AudioClip Background_intro;
+    public AudioClip Background_1stLoop;
     public AudioClip Background_Loop;
     public AudioClip Background_End;
     public AudioClip SFX_1;
@@ -19,6 +20,24 @@ public class Audio_Man : MonoBehaviour
     private void Start()
     {
         music.clip = Background_intro;
+        music.Play();
+    }
+
+    private void firstLoop()
+    {
+        music.clip = Background_1stLoop;
+        music.Play();
+    }
+
+    private void Loop()
+    {
+        music.clip = Background_Loop;
+        music.Play();
+    }
+
+    private void End()
+    {
+        music.clip = Background_End;
         music.Play();
     }
 
