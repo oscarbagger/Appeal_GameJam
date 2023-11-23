eee2f9e63b786c39fee3c88e6ebf04e64ed05453
oscarbagger <oscarbagger@live.dk>
2023-11-23 10:30:00 +0100

    Animated camera zoom

--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -6,12 +6,12 @@ public class CameraZoom : MonoBehaviour
 {
     public Vector3[] CameraPositions;
     private int CameraPositionIndex=0;
-    private AnimationCurve CameraAnimCurve;
+    [SerializeField] private float zoomTime;
+    [SerializeField] private AnimationCurve cameraAnimCurve;
 
     private void Start()
     {
         Camera.main.transform.position = CameraPositions[0];
-
     }
 
     public void NextCameraPosition()
@@ -19,7 +19,7 @@ public class CameraZoom : MonoBehaviour
         if (CameraPositionIndex<CameraPositions.Length-1)
         {
             CameraPositionIndex++;
-            Camera.main.transform.position = CameraPositions[CameraPositionIndex];
+            StartCoroutine(CameraMovement(CameraPositions[CameraPositionIndex - 1], CameraPositions[CameraPositionIndex], zoomTime));
         }
     }
     public void PreviousCameraPosition()
@@ -27,12 +27,20 @@ public class CameraZoom : MonoBehaviour
         if (CameraPositionIndex>0)
         {
             CameraPositionIndex--;
-            Camera.main.transform.position = CameraPositions[CameraPositionIndex];
+            StartCoroutine(CameraMovement(CameraPositions[CameraPositionIndex+1], CameraPositions[CameraPositionIndex], zoomTime));
         }
     }
 
-    private IEnumerator CameraMovement()
+    private IEnumerator CameraMovement(Vector3 start, Vector3 end, float time)
     {
-
+        float progress = 0f;
+        while (progress < time)
+        {
+            progress = progress + Time.deltaTime;
+            float percent = Mathf.Clamp01(progress / time);
+            float curvePercent = cameraAnimCurve.Evaluate(percent);
+            transform.position = Vector3.LerpUnclamped(start, end, curvePercent);
+            yield return null;
+        }
     }
 }
