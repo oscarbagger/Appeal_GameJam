9e1ebb57605b9ab8797be4caeb6c694948bd1fd6
Emil Madsen <Nesstark0@gmail.com>
2023-11-23 13:49:21 +0100

    Update PresentSpawner.cs

--- Assets/Scripts/PresentSpawner.cs
+++ Assets/Scripts/PresentSpawner.cs
@@ -6,13 +6,13 @@ public class Spawner : MonoBehaviour
 {
     public GameObject[] presents;
     private bool presentSpawned;
-    public Vector3 spawnPosition;
+    public Transform spawnLocation;
 
     void Update() {
         if (!presentSpawned && this.transform.childCount == 0) {
             presentSpawned = true;
             int choice = Random.Range(0, presents.Length);
-            Instantiate(presents[choice], spawnPosition, Quaternion.identity);
+            Instantiate(presents[choice], spawnLocation.position, Quaternion.identity);
         }
     } 
 }
