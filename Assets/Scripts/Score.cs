2c2d81941196f3ea3bea1d7af0a98259de120cec
Sofiethansen <147381788+Sofiethansen@users.noreply.github.com>
2023-11-23 12:11:14 +0100

    Update Score.cs

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    public Text m_MyText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_MyText.text = "this is my text";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(Pickup.OrnamentsOnTree);
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            m_MyText.text = "My text has now changed";
+        }
+
     }
 }
