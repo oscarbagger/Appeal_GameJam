c0ea75499d96de2671bdcf97d2662d104ca603e1
PhillzR <rumpelthiin1997@gmail.com>
2023-11-23 14:50:00 +0100

    IntroCutscene

--- Assets/Scripts/SantaLetter.cs
+++ Assets/Scripts/SantaLetter.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SantaLetter : MonoBehaviour
 {
+    [SerializeField] GameObject letterClosed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +16,11 @@ public class SantaLetter : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    public void LetterOpen()
     {
         
     }
